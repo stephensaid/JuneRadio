0a52ed6156af3085ef327386739ea1d7c86030fc
Stephen Said <stephen7776@gmail.com>
2021-02-16 23:53:02 +0100

    Fix GetInternetTime function
    Setting TimeZone using POSFIX

--- src/helpers.cpp
+++ src/helpers.cpp
@@ -417,7 +417,9 @@ int Start_WiFi() {
 void getInternetTime() {
   if ( WiFi.status() == WL_CONNECTED ) {
     waitForSync();
-    myTZ.setLocation(F("mt"));
+    // myTZ.setLocation(F("mt"));
+    // myTZ.setLocation("mt");
+    myTZ.setPosix(LOCALTZ_POSIX);
     Serial.print("getInternetTime::Updated internet time: ");
     Serial.println(myTZ.dateTime());
     myTZ.setDefault();
